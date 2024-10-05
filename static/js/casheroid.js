126b8fad6e30d14ee5d0a0a95879b3c355633208
Twilight Techie <da.kings.air@gmail.com>
2024-10-05 17:42:19 +0100

    Update Casheroid project

--- static/js/casheroid.js
+++ static/js/casheroid.js
@@ -11,4 +11,22 @@ function copyText() {
         console.error("Error in copying text: ", err);
       });
   }
-  
+
+  // JavaScript to add the spin animation on click
+  function spinFan() {
+    document.getElementById('fan-icon').addEventListener('click', function() {
+      const fanIcon = this;
+
+      // Add the spinning animation class
+      fanIcon.classList.add('spin-animation');
+
+      // Set a timeout for 10 seconds (10000ms)
+      setTimeout(function() {
+        // Remove the spin class to stop the animation
+        fanIcon.classList.remove('spin-animation');
+
+        // Trigger the alert after 10 seconds
+        alert('Congratulations! You won ₦200');
+      }, 5000); // 10000 milliseconds = 10 seconds
+    });
+  }
